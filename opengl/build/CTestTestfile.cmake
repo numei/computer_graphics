070f1252121a1218c8e919205bf73affc5a4c3c2
numei <2257894902@qq.com>
2026-01-21 14:31:53 +0100

    fix: clean build files

--- opengl/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mumei/Desktop/computer_graphics/opengl
-# Build directory: /Users/mumei/Desktop/computer_graphics/opengl/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
